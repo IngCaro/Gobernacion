a5678a8a1d6f38220912c905b027d241818d32e2
gobernacion <gobernacion@172.16.65.104>
2015-04-24 09:55:10 -0430

    nuevo avance

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-          echo "Hola muundo";
+          echo "Hola muun";
+          echo "siempre";
         ?>
     </body>
 </html>
